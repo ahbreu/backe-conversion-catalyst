bef94ca041fe0cd4424765167c40d573e209099b
ahbreu <pedroabreupmab@gmail.com>
2026-05-04 09:56:02 -0300

    fix: restore responsive public landing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,6 @@
 import BackeLandingReference from "@/components/BackeLandingReference";
-import MaintenancePage from "@/components/MaintenancePage";
-import { MAINTENANCE_MODE } from "@/config/maintenance";
 
 const Index = () => {
-  if (MAINTENANCE_MODE) {
-    return <MaintenancePage />;
-  }
-
   return <BackeLandingReference />;
 };
 
